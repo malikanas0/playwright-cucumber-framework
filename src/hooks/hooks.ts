3bc8078e6bdd5689ebadcaac2e9bc57f3ace2a5f
Malik Anas <malikanassultan@gmail.com>
2026-02-17 14:22:49 +0000

    TASK 7: Create Hooks

---
+++ src/hooks/hooks.ts
@@ -0,0 +1,13 @@
+import { Before, After } from "@cucumber/cucumber";
+import { chromium } from "@playwright/test";
+import { CustomWorld } from "../support/world";
+
+Before(async function (this: CustomWorld) {
+  this.browser = await chromium.launch({ headless: false });
+  this.context = await this.browser.newContext();
+  this.page = await this.context.newPage();
+});
+
+After(async function (this: CustomWorld) {
+  await this.browser.close();
+});
