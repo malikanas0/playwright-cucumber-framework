ad2a4d26684f3b9111f2809845f21d3ff537a860
Malik Anas <malikanassultan@gmail.com>
2026-02-18 11:54:19 +0000

    TASK 4 – Screenshot on Failure

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,6 +1,7 @@
 import { Before, After } from "@cucumber/cucumber";
 import { chromium } from "@playwright/test";
 import { CustomWorld } from "../support/world";
+import fs from "fs";
 
 Before(async function (this: CustomWorld) {
   this.browser = await chromium.launch({ headless: false });
@@ -8,6 +9,15 @@ Before(async function (this: CustomWorld) {
   this.page = await this.context.newPage();
 });
 
-After(async function (this: CustomWorld) {
+After(async function (this: CustomWorld, scenario) {
+  if (scenario.result?.status === "FAILED") {
+    if (!fs.existsSync("reports")) {
+      fs.mkdirSync("reports");
+    }
+    await this.page.screenshot({
+      path: `reports/${scenario.pickle.name}.png`
+    });
+  }
+
   await this.browser.close();
 });
